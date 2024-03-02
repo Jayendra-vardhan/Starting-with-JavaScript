93b8f3afe0607d25ebe9c906a937d10608fd7f22
Jayendra Vardhan Singh <jayendravardhan@gmail.com>
2024-03-02 20:37:01 +0530

    Tempary death zone

--- How JavaScript work/script.js
+++ How JavaScript work/script.js
@@ -1,24 +1,67 @@
-'use strict';
+//'use strict';
 
-function calcAge(birthyear){
-    const currentYear= 2024;
-    const age = currentYear-birthyear;
-    var millenial = false;
-    function printAge() {
-        const output=  `${firstName}, your age is ${age}, born in ${birthyear}`;
-        console.log(output);
+// function calcAge(birthyear){
+//     const currentYear= 2024;
+//     const age = currentYear-birthyear;
+//     var millenial = false;
+//     function printAge() {
+//         const output=  `${firstName}, your age is ${age}, born in ${birthyear}`;
+//         console.log(output);
         
-        if(birthyear>=1981 && birthyear<=1996){
-            millenial=!millenial;
-            const firstName='om';
-            const str = `oh, and you're a millenial, ${firstName}`; 
-            console.log(str);
-        }
-        console.log(millenial);
-    }
-    printAge();
-}
+//         if(birthyear>=1981 && birthyear<=1996){
+//             millenial=!millenial;
+//             const firstName='om';
+//             const str = `oh, and you're a millenial, ${firstName}`; 
+//             console.log(str);
+//         }
+//         console.log(millenial);
+//     }
+//     printAge();
+// }
 
 
-const firstName = 'jvs';
-calcAge(1996);
+// const firstName = 'deepasha';
+// calcAge(1996);
+/*****************************************************/
+//TDZ
+
+// console.log(me);
+// //console.log(job);
+// //console.log(year);
+
+// var me='Jonas';
+// let job='teacher';
+// const year=2002;
+
+// console.log(add(2,3));
+// // console.log(addExp(1,2));
+// // console.log(addArrw);
+
+// function add(a,b){
+//     return a+b;
+// }
+
+// const addExp=function(a,b){
+//     return a+b;
+// }
+
+// const addArrw=(a,b)=> a+b;
+
+
+// if(numPro) deletepro();
+
+// var numPro=10;
+
+// function deletepro(){
+//     console.log("delete all deepasha");
+// }
+
+// var x=1;
+// let y=2;
+// const z=3;
+
+// console.log(x===window.x);
+// console.log(y===window.y);
+// console.log(z===window.z);
+/**********************************************************/
+//
